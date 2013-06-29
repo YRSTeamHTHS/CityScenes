d6cb7cad68c9b69639208b117b213e8f161e52d6
matt hsu <mhsu@ctemc.org>
2013-06-29 17:38:07 -0400

    fixed two destination start/end address added horizontal divider under directions

--- js/maps/directions.js
+++ js/maps/directions.js
@@ -1,19 +1,22 @@
 var dir;
 $(document).ready(function() {
-$.getJSON('http://maps.googleapis.com/maps/api/directions/json?origin=Museum+Of+The+Moving+Image&destination=34+Ludlow+Street,NY&waypoints=30+Ludlow+St,NY|100+Canal+St,NY&sensor=false&mode=bicycling',function(data) {
+$.getJSON('http://maps.googleapis.com/maps/api/directions/json?origin=Museum+Of+The+Moving+Image&destination=34+Ludlow+Street,NY&sensor=false&mode=bicycling',function(data) {
 	//console.log(data.routes[0].legs[0].distance);
+	//http://maps.googleapis.com/maps/api/directions/json?origin=Museum+Of+The+Moving+Image&destination=34+Ludlow+Street,NY&waypoints=30+Ludlow+St,NY|100+Canal+St,NY&sensor=false&mode=bicycling
 	console.log(data);
-	var leg_distance = [];
-	var leg_duration = [];
-	var leg_start = [];
+	//var leg_distance = [];
+	//var leg_duration = [];
+	//var leg_start = [];
 	var leg_end = [];
+	var start_wrap = 	'<span>'+data.routes[0].legs[0].start_address+'<br /><br /></span>';
+	$(start_wrap).appendTo('div.directions');//begin directions formatting, start location
+	
 	$.each(data.routes[0].legs, function(i, legs){
-		leg_distance[i] = legs.distance.text;
-		leg_duration[i] = legs.duration.text;
-		leg_start[i] = legs.start_address;
+		//leg_distance[i] = legs.distance.text;
+		//leg_duration[i] = legs.duration.text;
+		//leg_start[i] = legs.start_address;
 		leg_end[i] = legs.end_address;
-		var leg_wrap = 	'<span>'+leg_start[i]+'<br /><br /></span>'+
-						'<ol class="directions">';
+		var leg_wrap = '<ol class="directions">';
 		//<span class="distance"></span><span class="duration"></span><span class="start"></span></li>'
 		console.log(legs.start_address);
 		$(leg_wrap).appendTo('div.directions');
@@ -25,7 +28,7 @@ $.getJSON('http://maps.googleapis.com/maps/api/directions/json?origin=Museum+Of+
 			var step_wrap = "<li>"+steps.html_instructions+'<br/><div class="distance" style="text-align:right">'+steps.distance.text+" - about "+steps.duration.text+"</div></li>";
 			$(step_wrap).appendTo('ol.directions');
 		})
-		leg_wrap = '<br /></ol>';
+		leg_wrap = '<br /></ol><span>'+leg_end[i]+'</span>';
 		$(leg_wrap).appendTo('div.directions');
 	});
 	
