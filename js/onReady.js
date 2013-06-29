606f910871f0beaa2e8be07f4708ffa737a7b9e2
Austin Eng <213reeses@gmail.com>
2013-06-29 16:32:58 -0400

    added input fields for sidebar

--- js/onReady.js
+++ js/onReady.js
@@ -1,3 +1,6 @@
+function updateRangeInput(val) {
+      document.getElementById('rangeTextVal').innerHTML=val; 
+}
 $(document).ready(function(){
 	/*$("#btn-side-menu-toggle").click(function(){
 		$("#side-menu").toggleClass("visible-menu");
